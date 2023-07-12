91720e471f693c5fb1ba3804e3bcd5776c071e7a
d pullaiah <pullaiah.dudekula@improve10x.com>
2023-07-12 17:44:38 +0530

    Completed Testing LessThenHundred task

---
+++ src/LessThenHundred.java
@@ -0,0 +1,12 @@
+public class LessThenHundred {
+
+    public boolean lessThenHundred(int a, int b) {
+        boolean result = false;
+        int sum = a + b;
+        int n = 100;
+        if (sum < n) {
+            result = true;
+        }
+        return result;
+    }
+}
---
+++ src/LessThenHundredTest.java
@@ -0,0 +1,35 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LessThenHundredTest {
+    private LessThenHundred lessThenHundred;
+
+    @BeforeEach
+    private void setUp() {
+        lessThenHundred = new LessThenHundred();
+    }
+
+    @Test
+    public void nothing() {
+    }
+
+    @Test
+    public void twentyTwo_fifteen_isLessThenHundred () {
+        boolean returnTrue = lessThenHundred.lessThenHundred(115, 25);
+        assertEquals(false, returnTrue);
+    }
+
+    @Test
+    public void eightyThree_thirtyFour_isLessThenHundred () {
+        boolean returnFalse = lessThenHundred.lessThenHundred(83, 34);
+        assertEquals(false, returnFalse);
+    }
+
+    @Test
+    public void three_seventySeven_isLessThenHundred () {
+        boolean returnTrue = lessThenHundred.lessThenHundred(83, 34);
+        assertEquals(false, returnTrue);
+    }
+}
