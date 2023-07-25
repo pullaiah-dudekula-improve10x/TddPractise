fe3a7e797c735e349f54cea561501c5ec8b97565
d pullaiah <pullaiah.dudekula@improve10x.com>
2023-07-25 16:11:07 +0530

    implemented another two testcases using TDD

--- src/templerun/ObstacleTest.java
+++ src/templerun/ObstacleTest.java
@@ -21,4 +21,17 @@ public class ObstacleTest {
         Obstacle obstacle = new Obstacle("", 20);
         assertEquals("", obstacle.getName());
     }
+
+    @Test
+    public void givenNameSpikePit_whenGetNameCalled_returnSpikePit() throws Obstacle.InvalidObstacleDamageException {
+        Obstacle obstacle = new Obstacle("Obstacle", 20);
+        assertEquals("Obstacle", obstacle.getName());
+
+    }
+    @Test
+    public void givenNameObstacleWithSpaces_whenGetNameCalled_returnSpikePit() {
+        Obstacle obstacle = new Obstacle("   Obstacle    ", 20);
+        assertEquals("Obstacle", obstacle.getName());
+    }
+
 }
