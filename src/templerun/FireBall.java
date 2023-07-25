3faaffade9dc78eb6eb21a6c15c15202eb645cee
d pullaiah <pullaiah.dudekula@improve10x.com>
2023-07-25 14:31:57 +0530

    created fireballTest cases

--- src/templerun/FireBall.java
+++ src/templerun/FireBall.java
@@ -1,12 +1,21 @@
 package templerun;
 
 public class FireBall {
+    private int speed;
     public FireBall(int speed) {
+        this.speed = speed;
         if(speed < 0 || speed > 100) {
             throw new InvalidFireBallSpeedException();
         }
 
     }
+
+    public void roll() {
+        System.out.println("FireBall is rolled with " + speed + " kmh speed!");
+
+    }
+
     public class InvalidFireBallSpeedException extends RuntimeException {
+
     }
 }
--- src/templerun/FireBallTest.java
+++ src/templerun/FireBallTest.java
@@ -1,17 +1,28 @@
 package templerun;
 
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 public class FireBallTest {
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    private void setUp() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
 
     @Test
     public void nothing() {
-
     }
 
     @Test
@@ -20,11 +31,26 @@ public class FireBallTest {
                 () -> new FireBall(-1),
                 "Fireball speed should be between 0 to 100");
     }
+
     @Test
     public void given101_throwsInvalidFireBallSpeedException() {
         assertThrows(FireBall.InvalidFireBallSpeedException.class,
                 () -> new FireBall(101),
                 "Fireball speed should be between 0 to 100");
+    }
+
+    @Test
+    public void given20_whenRollMethodCalled_displayRollingMessage() {
+        FireBall fireBall = new FireBall(10);
+        fireBall.roll();
+        assertEquals("FireBall is rolled with " + 10 + " kmh speed!", outputStream.toString().trim());
+
+    }
+    @Test
+    public void given99_whenRollMethodCalled_displayRollingMessage() {
+        FireBall fireBall = new FireBall(99);
+        fireBall.roll();
+        assertEquals("FireBall is rolled with " + 99 + " kmh speed!", outputStream.toString().trim());
     }
 }
 
